25ec7448a687bfce2600431a561fdda10b5d8b91
Lucas <lucas.sanner@codalia.fr>
2022-03-30 12:18:42 +0000

    Modifies the homepage.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,3 +1,8 @@
 
 <div class="h2">Bienvenue sur l'application.</div>
-<div class="col-12">Merci de bien vouloir vous <a href="{{ route('login') }}">connecter</a> pour accéder aux fonctionnalités de l'application.</div>
+
+ @auth
+    <div class="col-12">Vous pouvez maintenant accéder au tableau d'<a href="{{ route('operations.index') }}">opérations</a></div>
+ @else
+    <div class="col-12">Merci de bien vouloir vous <a href="{{ route('login') }}">connecter</a> pour accéder aux fonctionnalités de l'application.</div>
+ @endif
